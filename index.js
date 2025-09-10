340cad0a3183b6658cae4802a1c91d89df94ba64
jamiewilliamsxyz <jamie7williams@gmail.com>
2025-09-10 12:51:15 +0100

    Render colours fetched

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const schemeSelect = document.getElementById("scheme-select");
 const seedPicker = document.getElementById("seed-picker");
 const generateBtn = document.getElementById("generate-btn");
+const mainContainer = document.getElementById("main-container");
 
 let selectedScheme = "monochrome";
 let seedColour = "A3E3FF";
@@ -10,7 +11,7 @@ schemeSelect.addEventListener("change", () => {
 });
 
 seedPicker.addEventListener("change", (e) => {
-  seedColour = e.target.value.slice();
+  seedColour = e.target.value.slice(1);
 });
 
 generateBtn.addEventListener("click", () => {
@@ -20,5 +21,22 @@ generateBtn.addEventListener("click", () => {
 const getColourScheme = () => {
   fetch(
     `https://www.thecolorapi.com/scheme?hex=${seedColour}&mode=${selectedScheme}`
-  ).then((res) => res.json());
+  )
+    .then((res) => res.json())
+    .then((data) => renderColourScheme(data.colors));
+};
+
+const renderColourScheme = (data) => {
+  mainContainer.innerHTML = data
+    .map((colour) => {
+      return `
+      <div class="colour-column">
+        <div style="background-color:${colour.hex.value};" class="colour-strip"></div>
+        <div class="hex-container">
+          <p class="hex-text">${colour.hex.value}</p>
+        </div>
+      </div>
+    `;
+    })
+    .join("");
 };
