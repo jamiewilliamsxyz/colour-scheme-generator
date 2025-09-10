46d682ecd54ada491f54b692d7d9f4446fa13ef2
jamiewilliamsxyz <jamie7williams@gmail.com>
2025-09-10 11:42:38 +0100

    Handle selecting a seed colour and change seed picker/scheme select ids and names

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const schemeSelect = document.getElementById("scheme-select");
+const seedPicker = document.getElementById("seed-picker");
 
 let selectedScheme = "monochrome";
+let seedColour = "#A3E3FF";
 
 schemeSelect.addEventListener("change", () => {
   selectedScheme = schemeSelect.value;
-  console.log(selectedScheme);
+});
+
+seedPicker.addEventListener("change", (e) => {
+  seedColour = e.target.value;
 });
