34e08ba2933e9f03e4ce0b6f959afb473ccdea62
jamiewilliamsxyz <jamie7williams@gmail.com>
2025-09-10 12:20:00 +0100

    Add fetching a colour scheme based on the seed colour selected and the scheme selected

--- index.js
+++ index.js
@@ -3,14 +3,22 @@ const seedPicker = document.getElementById("seed-picker");
 const generateBtn = document.getElementById("generate-btn");
 
 let selectedScheme = "monochrome";
-let seedColour = "#A3E3FF";
+let seedColour = "A3E3FF";
 
 schemeSelect.addEventListener("change", () => {
   selectedScheme = schemeSelect.value;
 });
 
 seedPicker.addEventListener("change", (e) => {
-  seedColour = e.target.value;
+  seedColour = e.target.value.slice();
 });
 
-generateBtn.addEventListener("click", () => {});
+generateBtn.addEventListener("click", () => {
+  getColourScheme();
+});
+
+const getColourScheme = () => {
+  fetch(
+    `https://www.thecolorapi.com/scheme?hex=${seedColour}&mode=${selectedScheme}`
+  ).then((res) => res.json());
+};
